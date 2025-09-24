c05940414a10dbddfd8c892e9f7bb3187160e381
Alchemist625 <34391466+Alchemist625@users.noreply.github.com>
2025-09-24 14:57:23 +0330

    additional files added to .gitignore

--- tests/conftest.py
+++
@@ -1,8 +0,0 @@
-"""Configuration file for pytest."""
-import os
-import sys
-from pathlib import Path
-
-# Add the project root to the Python path
-project_root = str(Path(__file__).parent.absolute())
-sys.path.insert(0, project_root)
