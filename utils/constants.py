cfb5289bcfba1337c3979aec2a7d9f8b986161d9
Alchemist625 <34391466+Alchemist625@users.noreply.github.com>
2025-09-26 17:59:59 +0330

    tiktoken installed and added to the requirements

--- utils/constants.py
+++ utils/constants.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 JSON_CHUNK_VERSION = 1.0
-APP_NAME = "CSV PromptWise"
+APP_NAME = "CSV PromptWiser"
 DATA_FOLDER_NAME = "data"
 RESULTS_FOLDER_NAME = "results"
 RESULTS_DB_NAME = "processed_chunks.db"
@@ -45,16 +45,6 @@ MODEL_PREFS_DB_NAME = "model_prefs.db"
 DEFAULT_TEMPERATURE = 0.2
 DEFAULT_TOP_K = 40
 DEFAULT_TOP_P = 1.0
-
-
-# CLI constants
-APP_NAME_CLI = "PromptPilot"
-APP_DIR_CLI = os.path.join(DOCUMENTS_DIR, APP_NAME_CLI)
-DATA_DIR_CLI = os.path.join(APP_DIR_CLI, DATA_FOLDER_NAME)
-RESULTS_DIR_CLI = os.path.join(APP_DIR_CLI, RESULTS_FOLDER_NAME)
-TEMP_DIR_CLI = os.path.join(APP_DIR_CLI, TEMP_FOLDER_NAME)
-CONFIG_DIR_CLI = os.path.join(APP_DIR_CLI, CONFIG_FOLDER_NAME)
-MODEL_PREFS_DB_PATH_CLI = os.path.join(CONFIG_DIR_CLI, MODEL_PREFS_DB_NAME)
 
 # Model token limits (maximum context window sizes)
 MODEL_TOKEN_LIMITS = {
